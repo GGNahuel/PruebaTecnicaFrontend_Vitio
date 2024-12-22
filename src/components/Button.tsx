6b362bca5a7340c5b96eb9fc4b1feeabc2d7b28e
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-12-22 20:34:40 -0300

    styles to button and dialog components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,8 +4,10 @@ export default function Button({children, additionalClasses, ...attrs} :
   {children: ReactNode, additionalClasses?: string} & React.ButtonHTMLAttributes<HTMLButtonElement>) 
 {
   const classes = `
-    flex justify-center rounded-md bg-sky-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-sky-500 
+    flex justify-center rounded-md bg-sky-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm 
+    hover:bg-sky-500 active:bg-sky-700
     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600
+    box-border
   `
 
   return (
--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -4,12 +4,14 @@ import Button from "./Button";
 const Dialog = forwardRef<HTMLDialogElement, {headerChildren: ReactNode, children: ReactNode, handleDialog: () => void}>(
   ({headerChildren, children, handleDialog}, ref) => {
     return (
-      <dialog ref={ref}>
-        <header className="bg-plate-300 border-b-2">
+      <dialog ref={ref} className="rounded-md">
+        <header className="bg-plate-300 border-b-2 flex justify-between items-center mb-4 p-4">
           {headerChildren}
-          <Button onClick={handleDialog}>x</Button>
+          <Button onClick={handleDialog} additionalClasses="aspect-square">x</Button>
         </header>
-        {children}
+        <div className="p-4">
+          {children}
+        </div>
       </dialog>
     )
   }
