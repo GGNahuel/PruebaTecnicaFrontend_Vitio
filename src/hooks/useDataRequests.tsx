2daa4f5eeea30bd1f659ab60f1167bbc5c76824c
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-12-22 14:07:58 -0300

    extraction of addTask to a hook

---
+++ src/hooks/useDataRequests.tsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { ContextInterface, LocalStorageContext } from "../contexts/LocalStorageContext";
+import { TaskType } from "../types/TaskTypes";
+
+export function useAddTaskToList() {
+  const {setLocalStorageData} = useContext(LocalStorageContext) as ContextInterface
+
+  const addTask = (ev: React.FormEvent<HTMLFormElement>) => {
+    ev.preventDefault()
+
+    const formData = new FormData(ev.currentTarget)
+    const group = formData.get("group") as string
+    const newTask : TaskType = {
+      title: formData.get("title") as string,
+      group,
+      state: "process"
+    }
+
+    setLocalStorageData(prev => ({
+      tasks: {
+        ...prev.tasks,
+        [group]: {
+          ...prev.tasks[group],
+          listOfTasks: [...prev.tasks[group].listOfTasks, newTask]
+        }
+        }
+      }
+    ))
+  }
+
+  return {addTask}
+}
